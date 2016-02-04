ac3124751e3cd88313365c7919492776ae67b104
Andrew Theken <theken.1@osu.edu>
2016-02-04 12:25:19 -0500

    Correct duplication error.

--- ObjectiveGit/GTRemote.m
+++ ObjectiveGit/GTRemote.m
@@ -222,9 +222,9 @@ NSString * const GTRemoteRenameProblematicRefSpecs = @"GTRemoteRenameProblematic
 - (BOOL)addPushRefspec:(NSString *)pushRefspec error:(NSError **)error {
 	NSParameterAssert(pushRefspec != nil);
 
-	if ([self.pushRefspecs containsObject:fetchRefspec]) return YES;
+	if ([self.pushRefspecs containsObject:pushRefspec]) return YES;
 
-	int gitError = git_remote_add_push(self.repository.git_repository, self.name.UTF8String, fetchRefspec.UTF8String);
+	int gitError = git_remote_add_push(self.repository.git_repository, self.name.UTF8String, pushRefspec.UTF8String);
 	if (gitError != GIT_OK) {
 		if (error != NULL) {
 			*error = [NSError git_errorFor:gitError description:@"Failed to add push refspec."];
